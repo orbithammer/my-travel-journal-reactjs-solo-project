69066af7a3f26be080f16293d6d1e4988418aca9
Brock Penner <orbithammer@users.noreply.github.com>
2023-02-18 09:42:28 +0900

    Made components folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import data from "./data"
-import Header from "./Header"
-import Card from "./Card"
+import Header from "./components/Header"
+import Card from "./components/Card"
 
 export default function App() {
     const cards = data.map(card=>{
---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header>
+            <img src="./images/icon.png" />
+            <p>my travel journal.</p>
+        </header>
+    )
+}
